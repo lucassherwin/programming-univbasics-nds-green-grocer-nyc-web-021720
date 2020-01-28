4d784bbc95c482dc8f9000589bd863953cd6b63b
Lucas Sherwin <lksherwin@gmail.com>
2020-01-28 21:58:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -60,7 +60,7 @@ def apply_coupons(cart, coupons)
     discount_item =  find_item_by_name_in_collection(coupon[:item], cart)
     in_basket = !!discount_item
     can_apply = in_basket && discount_item[:count] >= coupon[:num]
-    
+
     if discount_item && can_apply
       apply_coupon_to_cart(discount_item, coupon, cart)
     end
