0d6d08c3b6de457d7cc4dfabbd92edb45a767502
Lucas Sherwin <lksherwin@gmail.com>
2020-01-28 21:49:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -42,6 +42,12 @@ def make_coupon_hash(coupon)
     :price => unit_price,
     :count => coupon[:num]
   }
+end
+def apply_coupon_to_cart(matching_item, coupon, cart)
+  matching_item[:count] -= coupon[:num]
+  item_with_coupon = mk_coupon_hash(coupon)
+  item_with_coupon[:clearance] = matching_item[:clearance]
+  cart << item_with_coupon
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
